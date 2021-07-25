53a4b402e48b966c2b14ca86fd729c71451325d0
OdalaT <43067037+Otlhomame@users.noreply.github.com>
2021-07-25 19:02:56 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,42 @@
+x= 7
+y= 7
+
+# if x>y:
+#   print("Yes")
+
+#===========================
+# x= 15
+# y= 7
+
+# if x>y:
+#   print("Yes")
+  
+# if "Hello" in "Hello world!":
+#   print("yes")
+
+# if x > y:
+#   print("Yes")
+# elif x < y:
+#   print("No its less")
+# else:
+#   print("neither") 
+
+#=========================== Nested If Statements
+
+# num= -3
+# if num>=0:
+#   if num==0:
+#     print("zero")
+
+#   else:
+#     print("Positive Number")
+
+# else:
+#   print("Negative Number")
+
+# #=========================== ONe line ifs
+# if 7>5 : print("Yes")
+
+#Ternary operator
+
+print("Yes") if 7>5 else print("No")
